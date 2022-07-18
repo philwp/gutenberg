715ef1dc6b767c653bec4a9251cf372ab95df944
Marco Ciampini <marco.ciampo@gmail.com>
2022-07-18 15:45:24 +0200

    Rename forwarded / merged refs

--- packages/components/src/popover/index.js
+++ packages/components/src/popover/index.js
@@ -118,7 +118,7 @@ const Popover = (
 		__unstableShift = false,
 		...contentProps
 	},
-	ref
+	forwardedRef
 ) => {
 	if ( range ) {
 		deprecated( 'range prop in Popover component', {
@@ -129,6 +129,7 @@ const Popover = (
 
 	const arrowRef = useRef( null );
 	const anchorRefFallback = useRef( null );
+
 	const isMobileViewport = useViewportMatch( 'medium', '<' );
 	const isExpanded = expandOnMobile && isMobileViewport;
 	const hasArrow = ! isExpanded && ! noArrow;
@@ -244,7 +245,6 @@ const Popover = (
 		bottom: 'top',
 		left: 'right',
 	}[ computedPlacement.split( '-' )[ 0 ] ];
-	const mergedRefs = useMergeRefs( [ floating, dialogRef, ref ] );
 
 	// Updates references
 	useLayoutEffect( () => {
@@ -333,6 +333,12 @@ const Popover = (
 			origin: placementToAnimationOrigin( computedPlacement ),
 		} );
 
+	const mergedFloatingRef = useMergeRefs( [
+		floating,
+		dialogRef,
+		forwardedRef,
+	] );
+
 	// Disable reason: We care to capture the _bubbled_ events from inputs
 	// within popover as inferring close intent.
 
@@ -350,7 +356,7 @@ const Popover = (
 				}
 			) }
 			{ ...contentProps }
-			ref={ mergedRefs }
+			ref={ mergedFloatingRef }
 			{ ...dialogProps }
 			tabIndex="-1"
 			style={
